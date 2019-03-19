c11021a7a83e0154784498423bc26964122b6813
Andrew Chen <andrew.chen@scotiabank.com>
2019-03-19 12:11:20 -0400

    added vue config

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+// vue.config.js
+
+module.exports = {
+  // ...
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 8082, // CHANGE YOUR PORT HERE!
+    https: false,
+    hotOnly: false
+  }
+  // ...
+}
